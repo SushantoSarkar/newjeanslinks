a39294d85e9927fe18113c8d9b5fe8f845dff93f
SushantoSarkar <112996911+SushantoSarkar@users.noreply.github.com>
2023-03-01 02:34:01 -0500

    Tweaks

--- assets/main.js
+++ assets/main.js
@@ -18,9 +18,32 @@ indow.arenaCallback = () => {
 //   event.currentTarget.classList.add("active");
 // }
 
+
+// function openTab(tabName) {
+//   var tabContent = document.getElementsByClassName("tab-pane");
+//   var tabButton = document.getElementsByClassName("filter-button");
+//   for (var i = 0; i < tabContent.length; i++) {
+//     if (tabContent[i].id === tabName) {
+//       tabContent[i].classList.add("active");
+//     } else {
+//       tabContent[i].classList.remove("active");
+//     }
+//   }
+//   for (var j = 0; j < tabButton.length; j++) {
+//     if (tabButton[j].classList.contains(tabName + "-button")) {
+//       tabButton[j].classList.add("active");
+//     } else {
+//       tabButton[j].classList.remove("active");
+//     }
+//   }
+//   return false;
+// }
+
+
 function openTab(tabName) {
   var tabContent = document.getElementsByClassName("tab-pane");
   var tabButton = document.getElementsByClassName("filter-button");
+  
   for (var i = 0; i < tabContent.length; i++) {
     if (tabContent[i].id === tabName) {
       tabContent[i].classList.add("active");
@@ -28,6 +51,7 @@ function openTab(tabName) {
       tabContent[i].classList.remove("active");
     }
   }
+  
   for (var j = 0; j < tabButton.length; j++) {
     if (tabButton[j].classList.contains(tabName + "-button")) {
       tabButton[j].classList.add("active");
@@ -35,5 +59,20 @@ function openTab(tabName) {
       tabButton[j].classList.remove("active");
     }
   }
+  
+  if (tabName === 'performances') {
+    document.getElementsByClassName("performances-button")[0].classList.add("active");
+    document.getElementsByClassName("soundscapes-button")[0].classList.remove("active");
+    document.getElementsByClassName("social-links-button")[0].classList.remove("active");
+  } else if (tabName === 'soundscapes') {
+    document.getElementsByClassName("performances-button")[0].classList.remove("active");
+    document.getElementsByClassName("soundscapes-button")[0].classList.add("active");
+    document.getElementsByClassName("social-links-button")[0].classList.remove("active");
+  } else if (tabName === 'social-links') {
+    document.getElementsByClassName("performances-button")[0].classList.remove("active");
+    document.getElementsByClassName("soundscapes-button")[0].classList.remove("active");
+    document.getElementsByClassName("social-links-button")[0].classList.add("active");
+  }
+  
   return false;
 }
